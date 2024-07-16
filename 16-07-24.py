1701d7c660d48fcecef628a400657bc97df6a9b1
sabitha7674 <165403727+sabitha7674@users.noreply.github.com>
2024-07-16 20:20:57 +0530

    Add files via upload

---
+++ 16-07-24.py
@@ -0,0 +1,28 @@
+'''#get value
+dict_={'python':56,'java':56.5,'perl':45,'css':True,45:'int'}
+for v in dict_.values():
+    print(v)'''
+#control statements
+'''#1.break
+i=1
+while i<10:
+    if i==5:
+        break
+    else:
+        print(i)
+        i+=1'''
+'''#2.continue
+#skip all the even numbers
+t=(34,5,3,68,4,90,77,23)
+for num in t:
+    if num%2==0:
+        continue
+    print(num)'''
+#pass
+for i in[4,5,6]:
+    pass
+for i in 'hai':
+    print(i)
+
+
+
